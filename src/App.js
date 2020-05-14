a0d305c4d53b82012bade30e23e7cd6c2caadc5b
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-14 14:19:11 -0700

    hard coded data renders to browser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    todos: [
+  {
+    todo: 'walk the dog',
+    completed: false
+  },
+  {
+    todo: 'do laundry',
+      completed: false
+  },
+  {
+    todo: 'go grocery shopping',
+      completed: false
+  }
+]
+  };
+
+
+
+  render() {
+    return (
+      <div className="App">
+<ul>
+{
+  this.state.todos.map(task => <li key={task.todo}>
+{task.todo}
+</li>)
+  }
+</ul>
+      </div>
+    )
+  }
 }
-
-export default App;
